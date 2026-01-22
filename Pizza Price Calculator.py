5c0ca8c767a6e57506f9a9f49e8365ca5ccfce9f
Moses <themosesafolabi@gmail.com>
2026-01-22 07:46:02 +0100

    I’ve got a job at Python Pizza! My first job is to build an automatic pizza order program.
    Here is the price list:
    Small pizza (S): $15
    Medium pizza (M): $20
    Large pizza (L): $25
    
    Small pizza plus pepperoni: +$2
    Medium or large pizza plus pepperoni: +$3
    
    Extra cheese for any size pizza: +$1

--- Pizza Price Calculator.py
+++ Pizza Price Calculator.py
@@ -1,7 +1,7 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+size = (input("What size pizza do you want? S, M or L: ")).upper()
+pepperoni = (input("Do you want pepperoni on your pizza? Y or N: ")).upper()
+extra_cheese = (input("Do you want extra cheese? Y or N: ")).upper()
 
 # How much they need to pay based on their size choice.
 if size == "S":
